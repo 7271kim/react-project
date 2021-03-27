6e9c17510f967a075d9c894cd828203d31fb4b7e
seokjin <7271kim@naver.com>
2021-03-27 14:36:16 +0900

    [2021.03.27] 개발시에도 IE 9 지원

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'react-app-polyfill/ie9';
+import 'react-app-polyfill/stable';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
